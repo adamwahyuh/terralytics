297e8736539a113e5ddb5d5a539cf5dadc22cd54
adam.wahyu.hariyadi <adam.wahyu.hariyadi@utpas.ac.id>
2025-08-30 21:21:17 +0700

    Hide Footer at dashboard/chat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,21 @@
 "use client";
 
 import Navbar from "@/components/Navbar";
+import { usePathname } from "next/navigation";
 import Footer from "@/components/Footer";
 import "./globals.css";
 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const hideFooter = pathname.startsWith("/dashboard/chat");
+
   return (
     <html lang="id">
       <body className="bg-green-50 font-sans">
         <Navbar />
         <main className="min-h-screen">{children}</main>
-        <Footer />
+        {!hideFooter && <Footer />}
       </body>
     </html>
   );
